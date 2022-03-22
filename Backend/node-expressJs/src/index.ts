a82521d34291bbba532018dc4be734aec0219311
iamimrankhan95 <iamimrankhan95@gmail.com>
2022-03-22 08:18:12 +0600

    runs ok

--- Backend/node-expressJs/src/index.ts
+++ Backend/node-expressJs/src/index.ts
@@ -1,4 +1,52 @@
-function sum (num1:number, num2:number){
-    return num1 + num2;
-}
-console.log(sum(10,6))
+import express, { Request, Response, Application, NextFunction } from 'express';
+
+const app: Application = express();
+const PORT = process.env.PORT || 8000;
+
+interface LocationWithTimezone {
+  location: string;
+  timezoneName: string;
+  timezoneAbbr: string;
+  utcOffset: number;
+};
+
+const getLocationsWithTimezones = (request: Request, response: Response, next: NextFunction) => {
+  let locations: LocationWithTimezone[] = [
+    {
+      location: 'Germany',
+      timezoneName: 'Central European Time',
+      timezoneAbbr: 'CET',
+      utcOffset: 1
+    },
+    {
+      location: 'China',
+      timezoneName: 'China Standard Time',
+      timezoneAbbr: 'CST',
+      utcOffset: 8
+    },
+    {
+      location: 'Argentina',
+      timezoneName: 'Argentina Time',
+      timezoneAbbr: 'ART',
+      utcOffset: -3
+    },
+    {
+      location: 'Japan',
+      timezoneName: 'Japan Standard Time',
+      timezoneAbbr: 'JST',
+      utcOffset: 9
+    }
+  ];
+
+  response.status(200).json(locations);
+};
+
+app.get('/timezones', getLocationsWithTimezones);
+
+app.get("/", (req: Request, res: Response): void => {
+  res.send("Hello Typescript with Node.js!")
+});
+
+app.listen(PORT, (): void => {
+  console.log(`Server Running here 👉 https://localhost:${PORT}`);
+});
