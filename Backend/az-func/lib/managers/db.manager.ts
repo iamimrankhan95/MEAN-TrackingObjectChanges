b147af8dbbe0b759a00b83c4adca8c139d1d22e8
iamimrankhan95 <iamimrankhan95@gmail.com>
2022-04-04 06:55:44 +0600

    init func app and add user func

---
+++ Backend/az-func/lib/managers/db.manager.ts
@@ -0,0 +1,48 @@
+
+import { Schema, model, connect } from "mongoose";
+
+class DbManager {
+    mongoDbconnString: string = "";
+    mongoDb = null;
+    static dbManager:DbManager;
+
+    constructor() {
+        // this.mongoDbconnString = connString;
+    }
+
+    static getManagerInstance() {
+        if (!this.dbManager) {
+            this.dbManager = new DbManager();
+        }
+
+        return this.dbManager;
+    }
+
+    async connectToDb(connString:string) {
+
+        if (!this.mongoDb) {
+            this.mongoDb = await this.connectToMongoDB(connString);
+        }
+    }
+
+    async connectToMongoDB(connString: string) {
+        return await connect(connString);
+    }
+
+    disconnect(){
+        
+    }
+
+    public findItemById = async (id) => {
+        return await CategoryModel.findById(id);
+    };
+}
+
+export default DbManager;
+
+const CategorySchema = new Schema(
+    { categoryName: String },
+    { timestamps: true }
+);
+
+const CategoryModel = model("Category", CategorySchema, "Bookstore");
